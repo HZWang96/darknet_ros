1a647e7ed053d3392087916c8369ee140a76a677
Haozhe Wang <wang_haozhe@u.nus.edu>
2021-09-05 16:08:56 +0800

    updated YoloObjectDetector.cpp to support yolov4

--- darknet_ros/src/YoloObjectDetector.cpp
+++ darknet_ros/src/YoloObjectDetector.cpp
@@ -297,7 +297,7 @@ detection* YoloObjectDetector::avgPredictions(network* net, int* nboxes) {
       count += l.outputs;
     }
   }
-  detection* dets = get_network_boxes(net, buff_[0].w, buff_[0].h, demoThresh_, demoHier_, 0, 1, nboxes);
+  detection* dets = get_network_boxes(net, buff_[0].w, buff_[0].h, demoThresh_, demoHier_, 0, 1, nboxes, 1);  // added input parameter '1'
   return dets;
 }
 
@@ -307,7 +307,7 @@ void* YoloObjectDetector::detectInThread() {
 
   layer l = net_->layers[net_->n - 1];
   float* X = buffLetter_[(buffIndex_ + 2) % 3].data;
-  float* prediction = network_predict(net_, X);
+  float* prediction = network_predict(*net_, X);  // changed from net_ to *net_
 
   rememberNetwork(net_);
   detection* dets = 0;
@@ -323,7 +323,7 @@ void* YoloObjectDetector::detectInThread() {
     printf("Objects:\n\n");
   }
   image display = buff_[(buffIndex_ + 2) % 3];
-  draw_detections(display, dets, nboxes, demoThresh_, demoNames_, demoAlphabet_, demoClasses_);
+  draw_detections_v3(display, dets, nboxes, demoThresh_, demoNames_, demoAlphabet_, demoClasses_, 1);  // changed from draw_detections() to draw_detections_v3() and added input parameter '1'
 
   // extract the bounding boxes and send them to ROS
   int i, j;
@@ -391,7 +391,9 @@ void* YoloObjectDetector::fetchInThread() {
 }
 
 void* YoloObjectDetector::displayInThread(void* ptr) {
-  int c = show_image(buff_[(buffIndex_ + 1) % 3], "YOLO", 1);
+  //int c = show_image(buff_[(buffIndex_ + 1) % 3], "YOLO", 1);
+  show_image_cv(buff_[(buffIndex_ + 1)%3], "YOLO");  // function changed because of change in image.h
+  int c = cv::waitKey(waitKeyDelay_); // due to change in image.h
   if (c != -1) c = c % 256;
   if (c == 27) {
     demoDone_ = 1;
